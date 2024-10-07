8eb8be263af6c244e66a6f4bd8582d9c4ef266be
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2024-10-06 23:47:28 -0400

    Create basic express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
