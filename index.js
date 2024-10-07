897aa9ec6c749684c99a20b504e76e790e9a0586
Ian Hafkenschiel <ihafkenschiel@gmail.com>
2024-10-06 23:52:55 -0400

    Add middlewares for logging and error handling

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const PORT = 3000;
 
 app.use(express.json());
 
+// Middleware to log each request
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
